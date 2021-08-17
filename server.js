a19a92fdb419286a9f0afe662b2d96cec78d90fd
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2021-08-16 21:35:35 -0700

    scaffolding complete

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = [];
+let projectDataIdx = 0;
 
 // Require Express to run server and routes
+var express = require('express')
 
 // Start up an instance of app
+var app = express()
+
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+var bodyParser = require('body-parser')
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+var cors = require('cors')
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+app.get('/getMostRecentEntry', (req,res) => {
+    if (projectData.length === 0) {
+        res.status(409).json({error: "Empty Data. None Exists."});
+    } else {
+        res.send(projectData[projectDataIdx]);
+        if (projectDataIdx > 0) projectDataIdx--;
+        console.log(projectDataIdx)
+    }
+})
+
+app.post('/addNewPost', async (req,res) => {
+    console.log(req.body)
+    projectData.push(req.body)
+    projectDataIdx = projectData.length - 1;
+    res.send({sucess : true})
+})
 
 // Setup Server
+const port = process.env.PORT || 5000;
+app.listen(port);
--- website/app.js
+++ website/app.js
@@ -1,5 +1,59 @@
 /* Global Variables */
+const apiKey = '20d9e6f5d90e787b833bc70f64e958dd'
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let date = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const getEnteredZipCode = () => document.getElementById('zip').value;
+
+const getEnteredFeelings = () => document.getElementById('feelings').value
+
+let testButton = document.getElementById('test-button');
+testButton.addEventListener('click', async (e) => {
+    e.preventDefault();  
+
+    const zipCode = getEnteredZipCode();
+    const feelings = getEnteredFeelings();
+
+    if (!zipCode && !feelings) console.log('not entered')
+
+    try {
+        let weatherData = await fetch (`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=imperial`)
+        if (!weatherData.ok) throw Error(weatherData.statusText);
+
+        weatherData = await weatherData.json()
+
+        const response = await fetch('/addNewPost', {
+            method: 'POST', 
+            mode: 'cors',
+            credentials: 'same-origin',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                date,
+                weatherData,
+                feelings
+            })
+        });
+    }
+    catch (e) {
+        console.log('error',e)
+    }
+})
+
+let generateButton = document.getElementById('generate');
+generateButton.addEventListener('click', async (e) => {
+    e.preventDefault();
+    try {
+        let res = await fetch('/getMostRecentEntry');
+        const error = !res.ok;
+        res = await res.json();
+        if (error) throw Error(res.error);
+        console.log(res)
+
+    } catch(e) {
+        console.log(e)
+    }
+
+})
